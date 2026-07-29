c78d4b8736cd6f16b20f4ea882509f29ba77d675
MB-Prabhu <prabhukumarmb@gmail.com>
2026-07-29 19:08:24 +0530

    added the school email

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,5 +2,5 @@
 // export const LOGO = "/org_logo.webp"
 export const LOGO = "/org_logo1.webp"
 
-export const SCHOOL_EMAIL = "ramspublicschool@gmail.com"
+export const SCHOOL_EMAIL = "ramstechcircle@gmail.com"
 export const SCHOOL_PHONENO = "7010957818"
